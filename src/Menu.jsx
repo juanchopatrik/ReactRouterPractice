b42b9a0d84a419ff1e7bbf39ed7ca64659a40801
juansilva <juandsr@hotmail.com>
2023-08-15 10:23:29 -0400

    agregando navlink al menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,10 +1,45 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Menu() {
     return (
-        <nav> Menu</nav>
+        <nav>
+            <ul>
+                {
+                    routes.map(route => (
+                        <li>
+                            <NavLink
+                                style={({ isActive }) => ({
+                                    color: isActive ? 'red' : 'blue',
+                                })}
+                                to={route.to}
+                            >
+                                {route.text}
+                            </NavLink>
+                        </li>
+                    ))
+                }
+            </ul>
+        </nav>
     )
 }
 
+const routes = []
+
+routes.push({
+    to: '/',
+    text: 'Home'
+})
+
+routes.push({
+    to: '/blog',
+    text: 'Blog'
+})
+
+routes.push({
+    to: '/profile',
+    text: 'Profile'
+})
 export { Menu }
